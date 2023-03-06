87f82dd9b06949d85ec5ed866a4d168824757724
Sunny <godfks7636@naver.com>
2023-03-06 14:19:08 +0900

    post부분 추가.

--- practice/react_foodboard/src/components/Cart/Cart.js
+++ practice/react_foodboard/src/components/Cart/Cart.js
@@ -9,6 +9,7 @@ import Checkout from "./Checkout";
     상단의 카트 버튼을 눌렀을 때 나오는 부분. Modal.js 로 타고 들어가면서 backdrop 창이 나온다.
  */
 const Cart = props => {
+    const localHost = `http://localhost:8080`;
     const [isCheckout, setIsCheckout] = useState(false);
     const cartCtx = useContext(CartContext);
 
@@ -25,6 +26,16 @@ const Cart = props => {
     const orderHandler = () => {
         setIsCheckout(current => !current);
     }
+
+    const submitOrderHandler = userData => {
+        fetch(`${localHost}/meal`, {
+            method: 'POST',
+            body: JSON.stringify({
+                user: userData,
+                orderedItems: cartCtx.items
+            })
+        });
+    };
 
     const cartItems =
         <ul className={styles['cart-items']}>{
@@ -54,7 +65,7 @@ const Cart = props => {
                 <span>Total Amount</span>
                 <span>{totalAmount}</span>
             </div>
-            {isCheckout && <Checkout onCancel={orderHandler}/>}
+            {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={orderHandler}/>}
             {!isCheckout && modalActions}
         </Modal>
     );
